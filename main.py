d0b7e4681e6a6370d202a6f88119e4f457c37a91
Krishna <kbhatgtr@gmail.com>
2026-09-04 21:35:19 +0400

    added backtracking

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import getFileInfo
 
 
 SCRIPT_DIR= Path(__file__).parent
-cachedDirs= []
+backwardDirCache= []
+forwardDirCache= []
+
+uniFont="Adwaita Sans"
 
 
 root= ctk.CTk()
@@ -18,6 +21,12 @@ root.grid_columnconfigure(0, weight=1)
 
 
 filesWindow=ctk.CTkScrollableFrame(root, width= 950, height=500)
+
+
+topBarFrame= ctk.CTkFrame(root, width=500, height=50,corner_radius=10)
+currentDirLabel= ctk.CTkLabel(topBarFrame, width=200, height=50,corner_radius=10,fg_color='transparent',font=(uniFont,15,"bold"))
+
+
 
 # Opening images
 folderIcon=Image.open(SCRIPT_DIR / "assets" / "folder.ico" )
@@ -34,13 +43,21 @@ applicationIcon=Image.open(SCRIPT_DIR / "assets" / "application.ico" )
 applicationIconForButton=ctk.CTkImage(light_image=applicationIcon,dark_image=applicationIcon,size=(84,84))
 
 
+backArrowIcon=Image.open(SCRIPT_DIR / "assets" / "backArrow.png" )
+backArrowIconForButton=ctk.CTkImage(light_image=backArrowIcon,dark_image=backArrowIcon,size=(32,32))
+
+forwardArrowIcon=Image.open(SCRIPT_DIR / "assets" / "forwardArrow.png" )
+forwardArrowIconForButton=ctk.CTkImage(light_image=forwardArrowIcon,dark_image=forwardArrowIcon,size=(32,32))
+
+
+
 
 def displayNewDirectory(parentDir, cRow=0, columnAmm=6, showHiddenFiles=False, buttonLength=140, isGoingBackward=False):
 
-    global cachedDirs
+    global backwardDirCache
 
 
-    if not isGoingBackward: cachedDirs.append(parentDir)
+    if not isGoingBackward: backwardDirCache.append(parentDir)
 
     if len(filesWindow.winfo_children()) > 0:
 
@@ -57,7 +74,7 @@ def displayNewDirectory(parentDir, cRow=0, columnAmm=6, showHiddenFiles=False, b
         absPath=parentDir + "/" + cItem
 
         fileLabel=ctk.CTkButton(filesWindow,
-            width=buttonLength, height=buttonLength, font=("Adwaita Sans",13,"bold"), corner_radius=10, text=cItem,
+            width=buttonLength, height=buttonLength, font=(uniFont,13,"bold"), corner_radius=10, text=cItem,
             anchor="s", fg_color="transparent", compound="top",
             command= lambda cFolder=cItem: displayNewDirectory(parentDir + "/" + cFolder))
 
@@ -77,25 +94,27 @@ def displayNewDirectory(parentDir, cRow=0, columnAmm=6, showHiddenFiles=False, b
             
         columnNumber=((i-1)%columnAmm)
         fileLabel.grid(column=columnNumber,row=cRow, pady=5, padx=5)
+
+        currentDirLabel.configure(text=parentDir)
         
         if i%columnAmm ==0:
             cRow+=1
 
 
 def displayBackwardsDirectory():
-    global cachedDirs
+    global backwardDirCache
 
-    displayNewDirectory(cachedDirs[-2], isGoingBackward=True)
+    displayNewDirectory(backwardDirCache[-2], isGoingBackward=True)
 
-    print(cachedDirs)
+    print(backwardDirCache)
 
-    try: cachedDirs.remove(cachedDirs[-1])
+    try: backwardDirCache.remove(backwardDirCache[-1])
     except: pass
 
-    #try: cachedDirs.remove(cachedDirs[-1])
+    #try: backwardDirCache.remove(backwardDirCache[-1])
     #except: pass
 
-    print(cachedDirs)
+    print(backwardDirCache)
 
     
 
@@ -105,10 +124,21 @@ displayNewDirectory("~")
 
 
 
-goBackwardsBtn= ctk.CTkButton(root, width=100, height=50, fg_color='transparent', text="<-",
+goBackwardsBtn= ctk.CTkButton(topBarFrame, width=80, height=50,corner_radius=10, fg_color='transparent',text='', image=backArrowIconForButton,
                 command=lambda: displayBackwardsDirectory())
 
-goBackwardsBtn.grid(row=0,sticky="sw")
+goForwardsBtn= ctk.CTkButton(topBarFrame, width=80, height=50,corner_radius=10, fg_color='transparent',text='', image=forwardArrowIconForButton,
+                command=lambda: displayBackwardsDirectory())
+
+
+
+goBackwardsBtn.grid(column=0,row=0,sticky="sw")
+goForwardsBtn.grid(column=1,row=0,sticky="sw")
+currentDirLabel.grid(column=3,row=0,sticky="sw")
+
+
+topBarFrame.grid(sticky="sw",row=0,column=0)
+
 filesWindow.grid(row=1,column=0,sticky="nsew")
 
 
